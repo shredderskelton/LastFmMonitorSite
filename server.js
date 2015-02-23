ed40292c5bbcda30f1cbc6a5db4293f67b03bc88
Nick Skelton <nick.skelton@sixt.com>
2015-02-23 13:30:46 +0100

    updated to express

--- server.js
+++ server.js
@@ -1,64 +1,66 @@
-var http = require('http');
+var express = require('express')
 var fs = require('fs');
+var http = require('http');
+var logfmt = require("logfmt");
+
+var app = express();
+app.use(logfmt.requestLogger());
 
 var lastPlayedResponse;
-
-var userNameToFollow = process.env.USERNAME;
+var userNameToFollow = process.env.USERNAMELASTFM;
 var api_key = process.env.APIKEY;
 
-http.createServer(function(req, res) {
+app.get('/nowplaying', function(req, res) {
+	res.writeHead(200, {
+		'Content-Type': 'text/json'
+	});
+	res.end(JSON.stringify(lastPlayedResponse));
+});
 
-	if (req.url == "/nowplaying") {
-		res.writeHead(200, {
-			'Content-Type': 'text/json'
-		});
-		res.end(JSON.stringify(lastPlayedResponse));
-		return;
-	}
-
+app.get('/index.html', function(req, res) {
+	console.log("request");
 	fs.readFile("index.html", function(err, text) {
 		res.setHeader("Content-Type", "text/html");
 		res.end(text);
 	});
-
-
-}).listen(1337, '127.0.0.1');
+});
 
 function requestNowPlayingFromLastFm() {
-
 	var options = {
-		host: "wp.sixt.de",
-		port: 8080,
 		path: 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + userNameToFollow + '&api_key=' + api_key + '&limit=1&format=json',
 		headers: {
 			Host: "ws.audioscrobbler.com"
 		}
 	};
 
+	if (process.env.NODE_ENV == 'production') {
+
+	} else {
+		options.host = "wp.sixt.de";
+		options.port = 8080;
+	}
 	callback = function(response) {
 		if (response.statusCode != 200) {
 			console.log("Error from server " + response.statusCode);
 		}
 		var str = '';
-		//another chunk of data has been recieved, so append it to `str`
 		response.on('data', function(chunk) {
 			str += chunk;
 		});
-		//the whole response has been recieved, so we just print it out here
 		response.on('end', function() {
-			// console.log(str);
 			lastPlayedResponse = JSON.parse(str);
-
-			// $("#txt").text(jsonResponse.recenttracks.track[0].artist);
 		});
 	};
 
 	http.request(options, callback).end();
-
 }
 
 setInterval(function() {
 	requestNowPlayingFromLastFm();
 }, 5000);
 
-console.log('Server running at http://127.0.0.1:1337/');
+var port = (process.env.PORT || 8080);
+
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
